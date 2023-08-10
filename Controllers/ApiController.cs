9bd1aa613267f7d93c9f51a8269e27e28a31a892
Burtnotbut <iurtk@outlook.com>
2023-08-10 09:42:21 +0800

    加入專案檔案。

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using MSIT150Site.Models;
+
+namespace MSIT150Site.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly DemoContext _context;
+        private readonly IWebHostEnvironment _host;
+
+
+        public ApiController(DemoContext context, IWebHostEnvironment host)
+        {
+            _context = context;
+            _host = host;
+
+        }
+
+        public IActionResult Index()
+        {
+            /*
+            System.Threading.Thread.Sleep(5000); 
+            //睡5秒鐘，再往下執行
+            */
+
+            return Content("Hello fetch!!");
+            //Content()是用來回傳字串
+        }
+
+        public IActionResult GetDemo(UserViewModel user) 
+        {
+            if ( string.IsNullOrEmpty(user.name) )
+            {
+                user.name = "guest";
+            }
+
+            return Content($"Hello {user.name}, you are {user.age} years old.");
+            //Content()是用來回傳字串
+        }
+
+        public IActionResult Register(Members member, IFormFile file) 
+        {
+          //_context.Members.Add(member);
+          //_context.SaveChanges();
+
+          //  return Content("新增成功");
+
+
+            string filePath = Path.Combine(_host.WebRootPath, "uploads", file.FileName);
+            //C:\shared\Ajax\MSIT150Site\wwwroot\uploads\walk.gif
+
+            //上傳檔案到uploads資料夾中
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+
+            //將圖轉成二進位
+            byte[]? imgByte = null;
+            using (var memoryStream = new MemoryStream())
+            {
+                file.CopyTo(memoryStream);
+                imgByte = memoryStream.ToArray();
+            }
+
+            //return Content($"上傳檔案到 {filePath}");
+
+            member.FileName = file.FileName;
+            member.FileData = imgByte;
+            _context.Members.Add(member);
+            _context.SaveChanges();
+
+            return Content("新增成功!!");
+        }
+
+        //作業試寫
+        public IActionResult CheckAccount(Members member)
+        {
+            return Content("新增成功!!");
+
+        }
+
+        //拿到照片回傳的方法
+        public IActionResult GetImageByte(int id = 1)
+        {
+            Members? member = _context.Members.Find(id);
+            byte[]? img = member.FileData;
+            return File(img, "image/jpeg");
+
+        }
+
+        //回傳城市的JSON資料
+        public IActionResult Cities()
+        {
+            var cities = _context.Address.Select(c => c.City).Distinct();
+            //去DemoContext找到Address資料表，撈取Select所有城市City欄位
+            //用Distinct()去除掉重複的部分
+
+            /*
+            var cities = _context.Address.Select(c => new
+            {
+                c.City
+            }).Distinct();
+            */
+
+            return Json(cities);
+            //回傳Json格式
+        }
+
+        //根據城市名稱，回傳城市的鄉鎮區JSON資料
+        public IActionResult Districts(string city)
+        {
+            var districts = _context.Address.Where(a => a.City == city).Select(c => c.SiteId).Distinct();
+            //去DemoContext找到Address資料表，撈取Select所有城市City欄位
+            //用Distinct()去除掉重複的部分
+
+            return Json(districts);
+            //回傳Json格式
+        }
+
+        //根據鄉鎮區名稱，回傳鄉鎮區的路名JSON資料
+        public IActionResult Roads(string siteId)
+        {
+            var roads = _context.Address.Where(a => a.SiteId == siteId).Select(c => c.Road).Distinct();
+
+            return Json(roads);
+            //回傳Json格式
+        }
+
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSIT150Site.Models;
+using System.Diagnostics;
+
+namespace MSIT150Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult First()
+        {
+            return View();
+        }
+
+        //旅遊景點介紹:作業1
+        public IActionResult Travel()
+        {
+            return View();
+        }
+
+        public IActionResult GetDemo()
+        {
+            return View();
+        }
+
+        //註冊頁面
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        //練習載入縣市資料的頁面
+        public IActionResult Address()
+        {
+            return View();
+        }
+
+        //練習Promise物件的頁面
+        public IActionResult Promise()
+        {
+            return View();
+        }
+
+        //練習fetch的頁面
+        public IActionResult Fetch()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
